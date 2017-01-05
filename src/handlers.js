926a1ec3f93c5a9c16b6733011bb8885714370c7
david90 <david.ng@oursky.com>
2017-01-05 19:46:22 +0800

    Add handler logic

--- src/handlers.js
+++ src/handlers.js
@@ -12,6 +12,44 @@ function webhookOrNull(slackUrl) {
   return new IncomingWebhook(slackUrl);
 }
 
+/**
+ * Show help.
+ */
+function showHelp() {
+  if (botConfig.debugMode) {
+    console.log('in showHelp');
+  }
+  const commands = [
+    'help - chima help'
+  ];
+  return {
+    text: commands.join('\n')
+  };
+}
+
 skygearCloud.handler('/slash-command', function (req) {
-  return 'yo';
+  if (botConfig.debugMode) {
+    console.log('in slash-command handler');
+  }
+
+  return new Promise((resolve, reject) => {
+    req.form(function (formError, fields) {
+      if (formError !== undefined && formError !== null) {
+        reject({error: formError});
+        return;
+      }
+
+      if (botConfig.debugMode) {
+        console.log('Received slash command with fields', fields);
+      }
+
+      if (fields.token !== botConfig.slackSlashCommandToken) {
+        console.error('slash command token does not match expected value');
+        reject({error: 'token does not match'});
+        return;
+      }
+
+      resolve({ text: 'Yeah' });
+    });
+  });
 });
