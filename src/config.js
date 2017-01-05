2f9a3ee23b469b78cc44cf6a1d6a5a1180ee449c
david90 <david.ng@oursky.com>
2017-01-05 19:39:39 +0800

    Update to read DEBUG_MODE env config

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 const botConfig = {
   slackSlashCommandToken: process.env.SLACK_SLASH_COMMAND_TOKEN,
   slackIncomingWebhook: process.env.SLACK_INCOMING_WEBHOOK,
-  debugMode: process.env.LUNCHBOT_DEBUG === 'true',
+  debugMode: process.env.DEBUG_MODE === 'true',
   appName: process.env.APP_NAME || '_',
   channelOverride: process.env.CHANNEL_OVERRIDE || '',
   defaultUser: process.env.DEFAULT_USER || 'admin',
