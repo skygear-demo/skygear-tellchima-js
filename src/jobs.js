dfd63ad9d4da8fd884fcb61c8bd22cde2f378123
david90 <david.ng@oursky.com>
2017-02-13 15:42:27 +0800

    Test Summary every 2 minutes

--- src/jobs.js
+++ src/jobs.js
@@ -60,7 +60,7 @@ function postSummary() {
  * Create a summary notification schedule interval.
  */
 // skygearCloud.every(botConfig.postSchedule, function () {
-skygearCloud.every('0 3 * * * *', function () { // Test version
+skygearCloud.every('0 */2 * * * *', function () { // Test version
   if (botConfig.debugMode) {
     console.log('in summary schedule cronjob');
   }
